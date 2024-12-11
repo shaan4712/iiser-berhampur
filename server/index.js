1a37f33fb5d77f4bd5c03fbea6cd143e02c73077
Shantanu Atmakuri <shaan2k3@gmail.com>
2024-12-12 00:38:16 +0530

    cors3

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // Move CORS before other middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://iiser-berhampur-git-main-a-shantanus-projects.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://iiser-berhampur.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
